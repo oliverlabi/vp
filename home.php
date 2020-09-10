27a18e4c92d157c99f9f88db7b9e1839a5545533
Oliver Labi <oliverlabi@users.noreply.github.com>
2020-09-10 19:28:47 +0300

    kodutöö

--- home.php
+++ home.php
@@ -14,14 +14,20 @@
   $semesterstart = new DateTime("2020-8-31"); 
   $semesterend = new DateTime("2020-12-13");
   //selgitame välja nende vahe ehk erinevuse
-  $semesterduration = $semesterstart->diff($semesterned);
+  $semesterduration = $semesterstart->diff($semesterend);
   //leiame selle päevade arvuna
   $semesterdurationdays = $semesterduration->format("%r%a");
-  
   //tänane päev
   $today = new DateTime("now");
+  $semesterdays = $semesterstart->diff($today);
+  $semesterdayssofar = $semesterdays->format("%r%a");
   //if($fromsemesterstartdays < 0){semester pole peale hakanud}
   //arvuta välja mitu protsenti õppetööst on tehtud nt: 9/105 * 100
+  $fromsemesterdurationdays = "Semester on alanud.";
+  if($today < $semesterdurationdays){
+	  $fromsemesterdurationdays = "Semester pole veel peale hakanud.";
+  }
+  $semesterpercentage = round(($semesterdayssofar / $semesterdurationdays) * 100, 1) . "%";
 ?>
 <!DOCTYPE html>
 <html lang="et">
@@ -36,5 +42,6 @@
   <p>Leht on loodud veebiprogrammeerimise kursuse raames <a href="http://www.tlu.ee">Tallinna Ülikooli</a> Digitehnoloogiate instituudis.</p>
   <p>Lehe avamise hetkel oli kuupäev ja kell: <?php echo $fulltimenow;?>.</p>
   <p><?php echo "Parajasti on " .$partofday ."."; ?> </p>
-  <p>Lõpetasin edukalt 1. nädala kodutöö - 17:34 02/09/20<p>
+  <p>1. nädala kodutöö - 17:34 02/09/20<p>
+  <p>2. nädala kodutöö - <?php echo $semesterpercentage;?> semestrist on läbitud.<p>
 </body>
